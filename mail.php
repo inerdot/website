e2ccc742a6f3cb6eebeea640df17574ae7b6e2bd
inerdot <franciscojosearioli@gmail.com>
2023-11-11 15:17:37 -0300

    Fomulario de contacto

--- mail.php
+++ mail.php
@@ -1,5 +1,4 @@
 <?php
-
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
@@ -9,28 +8,28 @@
 
         if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Completa el formulario e intenta de nuevo.";
             exit;
         }
-        $recipient = "nipabali56@gmail.com";
-        $subject = "New contact from $name";
+        $recipient = "inerdot6@gmail.com";
+        $subject = "Contacto de $name | INERDOT";
 
-        $email_content = "Name: $name\n";
+        $email_content = "Nombre: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
-        $email_content .= "Message:\n$message\n";
-        $email_headers = "From: $name <$email>";
+        $email_content .= "Asunto: $subject\n\n";
+        $email_content .= "Mensaje:\n$message\n";
+        $email_headers = "De: $name <$email>";
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Gracias! Su mensaje ha sido enviado. Nos pondremos en contacto en la brevedad.";
         } else {
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Error! Algo ha salido mal al intentar enviar tu mensaje.";
         }
 
     } else {
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Ha ocurrido un error! Por favor intenta de nuevo.";
     }
 
 ?>
